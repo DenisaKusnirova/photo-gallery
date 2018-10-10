fe19cd52e5b88a1138ddcad2c3a99bcdd6a5a025
Deniska Kusnirova <deniskakusnirova@gmail.com>
2018-10-10 12:34:46 +0200

    Render progressive image when loading & change favicon

--- src/components/photoDetail/PhotoDetail.js
+++ src/components/photoDetail/PhotoDetail.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import './photoDetail.scss'
+import ProgressiveImage from 'react-progressive-image'
+import placeholder from '../../resources/placeholder.png'
 
 const PhotoDetail = (props) => (
   <div className="photo-detail-container">
-    <div style={{ width: '50%' }}>
+    <div style={{ width: '90%' }}>
       <button onClick={props.handleCloseDetail} className="close-button">X CLOSE</button>
       <div className="photo-detail-flexbox">
         <button onClick={props.renderPrevious} className="prev-btn">{"<"}</button>
         <div className="white-container-photo">
-          <img src={props.photoPath} className="photo-detail" />
+            <ProgressiveImage src={props.photoPath} placeholder={placeholder}>
+              {src => <img src={src} alt="detail" className="photo-detail"/>}
+            </ProgressiveImage>
         </div>
         <button onClick={props.renderNext} className="prev-btn">></button>
       </div>
